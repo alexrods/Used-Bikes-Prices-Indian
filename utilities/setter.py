6c275ac8b4bdd65dcf4c2dc8e1d0f8bf01d19f82
alexrods <alexrodsanch@gmail.com>
2021-12-30 23:12:11 -0600

    fixerror in setter.py

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,7 +4,7 @@ from base64 import b64decode
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json', 'w') as json_file:
-        json_file.wrtie(b64decode(key.decode()))
+        json_file.wrtie(b64decode(key).decode())
     print(os.path.realpath('patn.json'))
 
 
