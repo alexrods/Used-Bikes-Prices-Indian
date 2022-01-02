97a38c74a0ded26a59e8d7b7dfc0a9b55bb40518
alexrods <alexrodsanch@gmail.com>
2022-01-01 22:35:21 -0600

    Add continuous training workflow

---
+++ src/train.py
@@ -0,0 +1,89 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+
+import sys
+import logging
+import numpy as np
+import pandas as pd
+
+from utils import update_model, save_simple_metrics_report, get_model_performance_test_set
+
+import warnings 
+warnings.filterwarnings('ignore')
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
+    level=logging.INFO,
+    datefmt='%H:%M:%S',
+    stream=sys.stderr
+)
+
+logger = logging.getLogger(__name__)
+
+logging.info('Loading data...')
+data = pd.read_csv('data/processed/bikes_processed.csv')
+logging.info('Loaded data...')
+
+cat_features = ['brand_name', 'model_name']
+cat_transformer = OneHotEncoder(handle_unknown='ignore')
+
+num_features = ['motor_size', 'years', 'kms_driven',
+                'mileage', 'power']
+num_transformer = StandardScaler()
+
+column_transform = ColumnTransformer(
+    transformers=[
+        ("num", num_transformer, num_features),
+        ("cat", cat_transformer, cat_features)
+    ]
+)
+
+logging.info("Separating dataset into train and test...")
+X = data.drop(['price'], axis=1)
+y = data['price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+model = Pipeline([('col_trans', column_transform),
+                 ('regressor', RandomForestRegressor())])
+
+logger.info("Setting Hyperparameters to tune...")
+param_tuning = [
+                {
+                'regressor': [GradientBoostingRegressor()],
+                'regressor__n_estimators': range(100, 1201, 100)
+                }
+               ]  
+
+grid_search = GridSearchCV(model, param_tuning, scoring='r2', cv=6, n_jobs=-1)
+
+logger.info("Starting grid search...")
+grid_search.fit(X_train, y_train)
+
+logger.info("Cross validating with the best model...")
+final_result = cross_validate(grid_search.best_estimator_, X_train, y_train, return_train_score=True, cv=5)
+
+train_score = np.mean(final_result['train_score'])
+test_score = np.mean(final_result['test_score'])
+assert train_score > 0.7
+assert test_score > 0.65
+
+logger.info(f"Train Score: {train_score}")
+logger.info(f"Test Score: {test_score}")
+
+logger.info("Updating model...")
+update_model(grid_search.best_estimator_)
+
+logger.info("Generating model report...")
+validation_score = grid_search.best_estimator_.score(X_test, y_test)
+save_simple_metrics_report(train_score, test_score, validation_score, grid_search.best_estimator_)
+
+y_test_pred = grid_search.best_estimator_.predict(X_test)
+get_model_performance_test_set(y_test, y_test_pred)
+
+logger.info("Training Finished!")
+
---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+from joblib import dump
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+
+
+def update_model(model: Pipeline) -> None:
+    dump(model, 'models/model.pkl')
+
+
+def save_simple_metrics_report(train_score: float, test_score: float,
+                               validation_score: float, model: Pipeline
+                               ) -> None:
+    with open('report.txt', 'w') as report_file:
+        report_file.write("# Model Pipeline Description"+"\n")
+
+        for key, value in model.named_steps.items():
+            report_file.write(f"### Train Score: {train_score} \n")
+            report_file.write(f"### Test Score: {test_score} \n")
+            report_file.write(f"### Validation Score: {validation_score} \n")
+
+
+def get_model_performance_test_set(y_real: pd.Series, y_pred: pd.Series) -> None:
+    fig, ax = plt.subplots()
+    fig.set_figheight(8)
+    fig.set_figwidth(8)
+    sns.regplot(x=y_real, y=y_pred, ax=ax)
+    ax.set_ylabel("Predicted Bike Price")
+    ax.set_xlabel("Real Bike Price")
+    ax.set_title("Behavior of model prediction")
+    fig.savefig('prediction_behavior.png')
+    
